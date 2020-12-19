80e965c06b10e0fd761f9797d987ff34e4b24b01
lixiaochen <lixiaochen@meishubao.com>
2020-12-19 14:15:32 +0800

    企业微信添加成员

--- src/test/java/com/web/selenium/testcase/WeiXinTest.java
+++ src/test/java/com/web/selenium/testcase/WeiXinTest.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,6 +15,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -53,10 +55,20 @@ public class WeiXinTest {
         cookies.forEach(cookieMap->{
             driver.manage().addCookie(new Cookie(cookieMap.get("name").toString(),cookieMap.get("value").toString()));
         });
+//      刷新界面
         driver.navigate().refresh();
         Thread.sleep(4000);
-
+        //添加新的成员
+        driver.findElement(By.xpath("//span[@class='ww_indexImg ww_indexImg_AddMember']")).click();
+        driver.findElement(By.id("username")).sendKeys("小江山");
+        driver.findElement(By.id("memberAdd_english_name")).sendKeys("小江山");
+        Random r = new Random(1);
+        int num = r.nextInt(100);
+        String acctid = "admin" + num;
+        driver.findElement(By.id("memberAdd_acctid")).sendKeys(acctid);
+        driver.findElements(By.xpath("//a[@class='qui_btn ww_btn ww_btn_Blue js_btn_continue']")).get(1).click();
     }
+
     @AfterAll
     public static void tearDown(){
         driver.quit();
