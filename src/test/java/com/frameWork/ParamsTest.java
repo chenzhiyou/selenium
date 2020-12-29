f4f14a7fe99e616243d1789cdf45b71bd21b09ca
lixiaochen <lixiaochen@meishubao.com>
2020-12-29 21:23:26 +0800

    测试步骤驱动引入

--- src/test/java/com/frameWork/ParamsTest.java
+++ src/test/java/com/frameWork/ParamsTest.java
@@ -4,16 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.core.type.TypeReference;
 import org.aspectj.util.FileUtil;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -35,43 +32,45 @@ public class ParamsTest {
         return Stream.of("apple", "banana");
     }
 
-    @ParameterizedTest
+//    @ParameterizedTest
 //    @ValueSource(strings = {"searche1", "shearch2"})
-    @MethodSource()
+//    @MethodSource()
     //在MethodSource后面不添加方法时，直接寻找同名的
-    public void search(String keyword){
-        ChromeDriver driver= new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.get("https://www.baidu.com");
-        driver.findElement(By.name("wd")).sendKeys(keyword);
+//    public void search(String keyword){
+    // 测试数据-数据驱动的方法
+//        ChromeDriver driver= new ChromeDriver();
+//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//        driver.get("https://www.baidu.com");
+//        driver.findElement(By.name("wd")).sendKeys(keyword);
+//    }
+    @ParameterizedTest
+    @MethodSource()
+    public void search(TestCase testCase){
+        //解析测试步骤数据驱动
+        System.out.println(testCase);
+        // done:Runner引擎
+        testCase.run();
     }
 
 //    public static Stream<String> search(){
 //        return Stream.of("apple", "banana");
 //    }
-    public static List<String> search() throws IOException {
+    // 测试数据数据驱动
+//    public static List<String> search() throws IOException {
+//        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+//        TypeReference typeReference = new TypeReference<List<String>>(){
+//        };
+//
+//        List<String> keywords= (List<String>) mapper.readValue(
+//                ParamsTest.class.getResource("/framework/search.yaml"),typeReference);
+//        return keywords;
+//    }
+    //测试步骤数据驱动
+    public static Stream<TestCase> search() throws IOException{
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        String path = ParamsTest.class.getResource("/framework/search.yaml").getPath();
-        System.out.println(path);
-        File file = new File(
-                String.valueOf(Thread.currentThread().
-                        getContextClassLoader().
-                        getResource("/framework/search.yaml")));
-        TypeReference typeReference = new TypeReference<List<String>>(){
-        };
-        System.out.println(FileUtil.canReadFile(file));
-        List<String> x = new ArrayList<String>();
-        x.add("xx");
-        x.add("ddd");
-        System.out.println(mapper.writeValueAsString(x));
-        List<String> keywords= (List<String>) mapper.readValue(
-                ParamsTest.class.getResource("/framework/search.yaml"),typeReference);
-        return keywords;
+        TestCase testCase = mapper.readValue(
+                ParamsTest.class.getResourceAsStream("/framework/search.yaml"),TestCase.class
+        );
+        return Stream.of(testCase);
     }
-
-
-
-
-
-
 }
