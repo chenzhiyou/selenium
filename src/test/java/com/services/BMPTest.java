3b4fb406cabc4add0305f39a093a2195f1eace03
lixiaochen <lixiaochen@meishubao.com>
2021-01-01 18:14:41 +0800

    追加依赖

--- src/test/java/com/services/BMPTest.java
+++ src/test/java/com/services/BMPTest.java
@@ -1,4 +1,16 @@
 package com.services;
 
+import net.lightbody.bmp.BrowserMobProxy;
+import net.lightbody.bmp.BrowserMobProxyServer;
+import org.junit.jupiter.api.Test;
+
 public class BMPTest {
+
+    @Test
+    public void bmp(){
+        BrowserMobProxy proxy = new BrowserMobProxyServer();
+        proxy.start(0);
+        int port = proxy.getPort();
+    }
+
 }
