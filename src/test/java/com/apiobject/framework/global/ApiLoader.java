d0e6aad544ca70f389dab52b75e48160076d2d85
lixiaochen <lixiaochen@meishubao.com>
2021-01-06 17:41:54 +0800

    增加getAction方法

--- src/test/java/com/apiobject/framework/global/ApiLoader.java
+++ src/test/java/com/apiobject/framework/global/ApiLoader.java
@@ -1,6 +1,10 @@
 package com.apiobject.framework.global;
 
+import com.apiobject.framework.actions.ApiActionsModel;
 import com.apiobject.framework.api.ApiObjectModel;
+import com.wechatinterface.department.Demo_01_base;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,6 +19,7 @@ public class ApiLoader {
     /**
      * 加载所有api Object对象，保存到本列表中
      */
+    private static final Logger logger = LoggerFactory.getLogger(Demo_01_base.class);
     private static List<ApiObjectModel> apis = new ArrayList<>();
 
     public static void load(String dir){
@@ -27,6 +32,19 @@ public class ApiLoader {
                 e.printStackTrace();
             }
         });
+    }
+
+    public static ApiActionsModel getAction(String apiName, String actionName){
+        final ApiActionsModel[] apiActionsModel = {new ApiActionsModel()};
+        apis.stream().filter(api->api.getName().equals(apiName)).forEach(api->{
+            apiActionsModel[0] = api.getActions().get(actionName);
+        });
+        if(apiActionsModel[0]!=null){
+            return apiActionsModel[0];
+        }else {
+            logger.info("没有找到接口对象："+apiName +"中的action" +actionName);
+        }
+        return null;
     }
 }
 
--- src/test/java/com/apiobject/test/Test03_ApoLoaderTest.java
+++ src/test/java/com/apiobject/test/Test03_ApoLoaderTest.java
@@ -3,19 +3,30 @@ package com.apiobject.test;
 import com.apiobject.framework.api.ApiObjectModel;
 import com.apiobject.framework.global.ApiLoader;
 import com.wechatinterface.department.Demo_01_base;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Test03_ApoLoaderTest {
     private static final Logger logger = LoggerFactory.getLogger(Demo_01_base.class);
 
-    @Test
-    public void apiLoadertest() throws IOException {
+    @BeforeAll
+    public static void apiLoadertest() throws IOException {
         ApiLoader.load("src/test/resources/api");
         logger.info("debugger!");
+    }
+
+    @Test
+    public void getActionTest(){
+        ArrayList<String> actualParamter = new ArrayList<>();
+        actualParamter.add("wwdeaeef5a5bf87579");
+        actualParamter.add("mLYMwTK2Ygar71W8yztfXYbKUji2POJ63u5pkNpBMkc");
+        ApiLoader.getAction("tokenhelper", "getToken").run(actualParamter);
+
     }
 }
